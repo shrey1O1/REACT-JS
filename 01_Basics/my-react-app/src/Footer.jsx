68ddd9db20b03b0e5a974010865d372809ba8e96
Shrey Borkar <thisismyemailid109@gmail.com>
2026-01-12 10:02:26 +0000

    About Functional components

---
+++ 01_Basics/my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()}Website Name</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ 01_Basics/my-react-app/src/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header(){
+
+    return(
+        <Header>
+            <h1>
+                My website
+            </h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </Header>
+    )
+}
+
+export default Header
